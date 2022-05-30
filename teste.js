476af954c1ac60009178b6811d1ac979737d0b13
letbarros2 <leticiabarros99600512@gmail.com>
2022-05-30 13:06:01 -0300

    primeiro teste js

---
+++ teste.js
@@ -0,0 +1,15 @@
+// PRIMEIRO HELLO WORLD JS 
+// console.log('HELLO WORLD!');
+// _________________________________
+function returnEvenValues(array){
+    let evenNums = [];
+    for(let i =0; i < array.length; i++){
+        if(array[i] %2==0){
+            evenNums.push(array[i]);
+        }
+    }
+    console.log(evenNums);
+    console.log('os números pares são:', evenNums);
+        }
+   let array=[1,2,4,5,7,8];
+   returnEvenValues(array);    
