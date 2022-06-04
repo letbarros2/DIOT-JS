b281724be6dc1628bd4eeac672432111d109ed66
letbarros2 <leticiabarros99600512@gmail.com>
2022-06-04 14:51:47 -0300

    criando primeiro contador js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+// console.log('hello world')
+// criando variavel  ELEMENTO SPAN 
+var currentNumberWrapper = document.getElementById('currentNumber')
+var currentNumber = 0;
+
+//declarando função subtração
+function decrement(){
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    }
+// declarando função
+function increment(){
+currentNumber = currentNumber + 1;
+currentNumberWrapper.innerHTML = currentNumber;
+}
+
