8cdcbc089c86e2d3b4976004ad11d8352d82787d
letbarros2 <leticiabarros99600512@gmail.com>
2022-06-13 17:55:26 -0300

    EDIT

--- FUNCOES/script.js
+++ FUNCOES/script.js
@@ -1,4 +1,22 @@
+const alunos=[
+   {
+    nome:'Leticia',
+    nota: 8,
+    turma: '1B',
+},{
+    nome:'Leticia',
+    nota: 8,
+    turma: '1B',
+
+},{:'Leticia',
+nota: 8,
+turma: '1B',
+}
+];
+
 function aprovados(arr, media){
     let app = [];
-    for (let i = 0; i < arr.length; i++)
+    for (let i = 0; i < arr.length; i++){
+        if()
+    }
 }
