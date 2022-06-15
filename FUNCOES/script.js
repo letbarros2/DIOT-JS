7fdfbab3dfc020ad6c2dc0a73563acc2ce0582df
letbarros2 <leticiabarros99600512@gmail.com>
2022-06-14 22:03:53 -0300

    CALCULANDO MEDIA ALUNOS

--- FUNCOES/script.js
+++ FUNCOES/script.js
@@ -1,22 +1,37 @@
 const alunos=[
    {
     nome:'Leticia',
-    nota: 8,
+    nota: 9,
     turma: '1B',
-},{
-    nome:'Leticia',
-    nota: 8,
-    turma: '1B',
+},
+{
+    nome:'Liandra',
+    nota: 6,
+    turma: '2B',
 
-},{:'Leticia',
-nota: 8,
-turma: '1B',
-}
+},
+{ 
+    nome:'ROSA',
+    nota: 4,
+    turma: '1B',
+},
+{ 
+    nome:'a',
+    nota: 3,
+    turma: '1B',
+},
 ];
 
-function aprovados(arr, media){
-    let app = [];
+function Aprovados(arr, media){
+    let passar = [];
     for (let i = 0; i < arr.length; i++){
-        if()
+        const {nota,nome} = arr[i];
+
+
+        if(nota >= media){
+            passar.push(nome);
+        }
     }
+    return passar;
 }
+console.log(Aprovados(alunos,5));
