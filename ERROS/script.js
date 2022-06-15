38d7d6e854e98d30f0e105ac9fc7a96baaee7115
letbarros2 <leticiabarros99600512@gmail.com>
2022-06-14 23:17:08 -0300

    USANDO TRY CATCH PARA VALIDACAO

--- ERROS/script.js
+++ ERROS/script.js
@@ -8,3 +8,33 @@ function validandoArray(arr,num){
 
     if(arr.length !== 'num') throw new RangeError('Erro no tamanho');
 }
+
+function validandoArray2(arr,num){
+    try{
+        if(!arr && !num) throw new ReferenceError('Envie os parametros');
+
+        if(typeof arr !=='object') throw new TypeError('Array precisa ser do tipo object');
+    
+        if(typeof arr !=='number') throw new TypeError('Array precisa ser do tipo number');
+    
+        if(arr.length !== 'num') throw new RangeError('Erro no tamanho');
+    return arr;    
+    }
+    catch(e){
+        if(e instanceof ReferenceError){
+            console.log('Este erro é um referenceErro')
+            console.log(e.message)
+            }
+            else if (e instanceof TypeError){
+            console.log('Este erro é um TypeError')
+            console.log(e.message)
+        
+    }else if (e instanceof RangeError){
+    console.log('Este erro é um RangerError')
+    console.log(e.message)
+    }else{
+        console.log("Tipo de erro não esperado:"+ e);
+    }
+}
+}
+console.log(validandoArray2());
