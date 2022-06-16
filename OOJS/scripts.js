d48060d9c34cd191003513bf1a74e8d60e291d45
letbarros2 <leticiabarros99600512@gmail.com>
2022-06-16 12:01:42 -0300

    CLASS CONTA CORRENTE

--- OOJS/scripts.js
+++ OOJS/scripts.js
@@ -46,3 +46,16 @@ depositar(valor){
 
 }    
 }
+class contaCOrrente{
+    constructor(agencia, numero, saldo, cartaoCredito){
+        super(agencia,numero,saldo);
+        this.tipo = 'corrente';
+        this.cartaoCredito = cartaoCredito;
+    }
+    get cartaoCredito(){
+        return this._cartaoCredito;
+    }
+    set cartaoCredito(valor){
+        this._cartaoCredito = valor
+    }
+}
