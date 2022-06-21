a4416ddc9c2c1e6dcd2f80617472386c777dd2db
letbarros2 <leticiabarros99600512@gmail.com>
2022-06-20 22:11:02 -0300

    desafios dio

---
+++ desafio/NUMEROPAR.js
@@ -0,0 +1,13 @@
+
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+var array = [ 2, 3, 5, 7, 11, 13, 18, 34 ];
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+ 	const numPar = (num) => num % 2 === 0;
+for (let i = 0; i < array.length; i++){
+    if (numPar(array[i])  ){
+        print(array[i])
+     }          
+}
---
+++ desafio/SOMANUMEROS.js
@@ -0,0 +1,7 @@
+
+const A = parseInt;
+const B = parseInt;
+const total =  ((B-A + 1)*(A+B)/2);
+console.log(total);
+
+//Escreva o seu código no espaço em branco
